edb52e1754acdb21e29fe7eaf58b13a7ff330da5
GerardoFernandez7 <101151869+GerardoFernandez7@users.noreply.github.com>
2024-01-25 16:41:42 -0600

    Se agrego el agrego el algoritmo insertion sort

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,2 +1,23 @@
-public class InsertionSort {
+public class InsertionSort <T extends Comparable<T>> implements ISort<T> {
+
+    @Override
+    public T[] sort(T[] arr) {
+
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            T key = arr[i];
+            int j = i - 1;
+
+            // Mover los elementos mayores a key una posición adelante
+            while (j >= 0 && arr[j].compareTo(key) > 0) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+
+        return arr;
+    }
+
 }
+
--- Main.java
+++ Main.java
@@ -6,9 +6,15 @@ public class Main {
 
 
         GnomeSort sort = new GnomeSort();
-        sort.sort(DataManager.saveNumbers(10, "datos.txt"));
-
+        //sort.sort(DataManager.saveNumbers(10, "datos.txt"));
+        InsertionSort insertionsort = new InsertionSort();
+        //insertionsort.sort(DataManager.saveNumbers(10, "datos.txt"));
     }
 
-
+    private static <T> void imprimirArray(T[] array) {
+        for (T elemento : array) {
+            System.out.print(elemento + " ");
+        }
+        System.out.println();
+    }
 }
