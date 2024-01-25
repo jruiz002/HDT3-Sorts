696b1afa55c83dcbc2adcde04b6f646a167e03af
jruiz002 <josegrg04@gmail.com>
2024-01-25 15:43:50 -0600

    Se agregó el método de ordenamiento Gnome Sort.

--- GnomeSort.java
+++ GnomeSort.java
@@ -1,2 +1,19 @@
-public class GnomeSort {
+public class GnomeSort <T extends Comparable<T>> implements ISort<T>{
+
+    @Override
+    public T[] sort(T[] array) {
+        int index = 0;
+        while (index < array.length) {
+            if (index == 0 || array[index].compareTo(array[index - 1]) >= 0) {
+                index++;
+            } else {
+                // Swap elements if they are in the wrong order
+                T temp = array[index];
+                array[index] = array[index - 1];
+                array[index - 1] = temp;
+                index--;
+            }
+        }
+        return array;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola");
+        // Ejemplo de uso con un array de enteros
+        Integer[] numeros = {5, 2, 8, 1, 7, 3};
+
+        System.out.print("Array original: ");
+        imprimirArray(numeros);
+
+        GnomeSort gnomeSort = new GnomeSort();
+
+        gnomeSort.sort(numeros);
+
+        System.out.print("Array ordenado: ");
+        imprimirArray(numeros);
+    }
+
+    private static <T> void imprimirArray(T[] array) {
+        for (T elemento : array) {
+            System.out.print(elemento + " ");
+        }
+        System.out.println();
     }
 }
